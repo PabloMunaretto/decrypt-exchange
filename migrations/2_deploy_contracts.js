7b3fc654b5436a5253b31db3488c108c258d299e
pablo <pablo.munaretto@gmail.com>
2021-09-20 16:48:53 -0300

    deploying of contracts done ok

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,14 @@ const Exchange = artifacts.require("Exchange");
 
 // Migrate means changing the blockchain state into a new one
 
-module.exports = function (deployer) {
-  deployer.deploy(Token);
-  // deployer.deploy(Exchange);
+module.exports = async function(deployer) {
+  const accounts = await web3.eth.getAccounts();
+  const feeAcount = accounts[0];
+  const feePercent = 10;
+  
+  await deployer.deploy(Token);
+  await deployer.deploy(Exchange, feeAcount, feePercent);
 };
 
 // 1- truffle deploy creates abis from the contracts
-// 2- truffle migrate adds the the abis to the blockchain (it also runs deploy automatically)
+// 2- truffle migrate adds the abis to the blockchain (it also runs deploy automatically)
