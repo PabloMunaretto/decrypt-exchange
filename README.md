1a1f00b9922b1f02fa117ffa35f6b2b3a043b92d
pablo <pablo.munaretto@gmail.com>
2021-10-19 19:14:54 -0300

    project deployed on fantom testnet network

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Download the metamask browser extension
 
-## Go to https://gitter.im/kovan-testnet/faucet Or https://ethdrop.dev/ and write your wallet address in order to get fake ETH to interact with the Exchange
+## Configure testnet on your metamask browser extension (metamask config => id: 4002, rpc: "https://rpc.testnet.fantom.network/").
 
-### Make sure you are in the Kovan network in your metamask config
+### Go to https://faucet.fantom.network/ and put your metamask address account in order to receive some fake FTM for transaction fees.
+
+#### Make sure you are in the FTM testnet network in your metamask. 
+
+##### 
 
--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 const privateKeys = process.env.PRIVATE_KEYS || "";
 
@@ -23,6 +23,17 @@ module.exports = {
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 42
+    },
+    fantom: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(","), // Array of account private keys
+          "https://rpc.testnet.fantom.network/" // url to an Ethereum node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 4002
     }
   },
   contracts_directory: './src/contracts/',
